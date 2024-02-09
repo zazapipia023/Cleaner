ce9b9fdc2368abe894e5a7d39048958a9e257e1e
Zaza Pipia <zaza.pipia.023@gmail.com>
2024-02-09 19:29:56 +0300

    added directories

--- cleaner.py
+++ cleaner.py
@@ -19,9 +19,52 @@ def clean_dir(directory):
             print(f"Error on delete {file_path}: {e}")
 
 
+def clean_root_dir():
+    root_dirs = [r"D:\ChromeDownloads", r"D:\ColizeumUtil", r"D:\Games",
+                 r"D:\Игры", r"D:\Клубные аккаунты.lnk", r"D:\Игры без аккаунтов.lnk"]
+    root_dir = r"D:"
+    for file in os.listdir(root_dir):
+        file_path = os.path.join(root_dir, file)
+        if file_path in root_dirs:
+            continue
+        else:
+            try:
+                if os.path.isdir(file_path) and os.path.exists(file_path):
+                    shutil.rmtree(file_path)
+                    print(f"Deleted directory {file_path}")
+                if os.path.isfile(file_path) and os.path.exists(file_path):
+                    os.remove(file_path)
+                    print(f"Deleted file {file_path}")
+            except OSError as e:
+                print(f"Error on delete {file_path}: {e}")
+
+def clean_games_root_dir():
+    games_root_dirs = [r"D:\Games\Battle.net", r"D:\Games\BsgLauncher", r"D:\Games\Epic Games",
+                 r"D:\Games\Game Centre", r"D:\Games\Genshin Impact", r"D:\Games\Innova",
+                 r"D:\Games\Lesta", r"D:\Games\Minecraft", r"D:\Games\Portable", r"D:\Games\Riot Games",
+                 r"D:\Games\Rockstar Games", r"D:\Games\Steam", r"D:\Games\Tanki", r"D:\Games\Ubisoft Game Launcher",
+                       r"D:\Games\tarkov", r"D:\Games\RAGEMP"]
+    root_dir = r"D:\Games"
+    for file in os.listdir(root_dir):
+        file_path = os.path.join(root_dir, file)
+        if file_path in games_root_dirs:
+            continue
+        else:
+            try:
+                if os.path.isdir(file_path) and os.path.exists(file_path):
+                    shutil.rmtree(file_path)
+                    print(f"Deleted directory {file_path}")
+                if os.path.isfile(file_path) and os.path.exists(file_path):
+                    os.remove(file_path)
+                    print(f"Deleted file {file_path}")
+            except OSError as e:
+                print(f"Error on delete {file_path}: {e}")
+
+
 def clean_chrome_downloads():
     dir_chrome_downloads = r"D:\ChromeDownloads"
     clean_dir(dir_chrome_downloads)
+
 
 def clean_steam_workshop_content():
     dir_workshop_content = r"D:\Games\Steam\steamapps\workshop\content"
@@ -90,5 +133,7 @@ clean_chrome_downloads()
 clean_steam_workshop_content()
 clean_steam_downloading_content()
 clean_games()
+clean_games_root_dir()
+clean_games_root_dir()
 
 input("Нажмите Enter для выхода...")
