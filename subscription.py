d7c8310bccdc6bc412d1efa27c039c9d0630eef7
Zaza Pipia <zaza.pipia.023@gmail.com>
2024-03-28 20:28:57 +0300

    init commit

--- subscription.py
+++ subscription.py
@@ -4,6 +4,6 @@ import requests
 
 def check_subscription():
     logger.info("Checking subscription")
-    url = f"http://localhost:8080/subscription?clubId=id"
+    url = f"http://81.200.145.178:8080/subscription?clubId=141-0"
     r = requests.get(url)
     return r.text
